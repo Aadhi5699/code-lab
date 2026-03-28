d3ce27528ee13ae994f747acbccdcc49ccb32dd5
Aadhi5699 <aadhi5699@gmail.com>
2026-03-28 20:42:24 +0530

    String and sorting

---
+++ Sid.java
@@ -0,0 +1,11 @@
+class Kata {
+  public static String numberToString(int num) {
+    return Integer.toString(num); 
+  }
+}
+
+public class Sid {
+    public static int simpleMultiplication (int n) {
+        return n % 2 == 0 ? n * 8 : n * 9;
+    }
+}

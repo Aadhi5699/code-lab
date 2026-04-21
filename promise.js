66b0497683da8a02d7bcba3f13d509c59224a2f0
Aadhi5699 <aadhi5699@gmail.com>
2026-04-21 13:53:03 +0530

    rejected promise

---
+++ promise.js
@@ -0,0 +1,17 @@
+Promise.resolve()
+  .then(() => {
+    console.log("X"); 
+    throw new Error("Fail"); 
+  })
+  .then(() => console.log("Y"))
+  .catch((err) => console.log(err.message)) 
+  .then(() => console.log("Z")); 
+
+
+  Promise.reject("Error")
+  .then((res) => console.log(res))
+  .catch((err) => {
+    console.log(err);
+    return "Recovered";
+  })
+  .then((msg) => console.log(msg));

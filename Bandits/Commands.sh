31b3aa47eaebfbaee2cd0a37007717161f18eb9d
Aadhi5699 <aadhi5699@gmail.com>
2026-06-08 00:03:18 +0530

    using pipes

--- Bandits/Commands.sh
+++ Bandits/Commands.sh
@@ -11,3 +11,12 @@ ls -a
 
 #The file command examines a file and tells you what kind of data it contains.
 file ./*
+# c suffix stands for bytes
+find . -type f -size 1033c ! -executable
+# 2 = Standard Error , /dev/null is a special, empty file in Linux
+2>/dev/null
+
+# -u = Input: A, A, A, B, C, C output: B (Because A and C repeated, they are deleted entirely.
+# Strings just show human readable text, filters out the binary junk.
+sort data.txt | uniq -u
+strings data.txt | grep "=="

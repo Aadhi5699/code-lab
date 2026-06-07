532943b041abada6f9c72aa758d42d7709e8631b
Aadhi5699 <aadhi5699@gmail.com>
2026-06-07 19:13:01 +0530

    spaces and hidden files

--- Bandits/Commands.sh
+++ Bandits/Commands.sh
@@ -1,3 +1,10 @@
 # Open the file called - located in the current directory. 
 # This forces the command to treat the string as a path instead of an option.
 cat ./-
+
+# "" or \ could be used to treat this whole as a single filename.
+cat "./--spaces in this filename--"
+
+# Files beginning with a . are hidden. 
+#The -a flag means show all files, including hidden ones.
+ls -a

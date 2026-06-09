c1f0e93a9e2680f65c65f96aa75e3b47bcd54272
Aadhi5699 <aadhi5699@gmail.com>
2026-06-09 23:23:46 +0530

    Mock Data

--- NodeWild/server.js
+++ NodeWild/server.js
@@ -1,8 +1,11 @@
 import http from 'node:http'
 
 const PORT = 8000;
+
 const server = http.createServer((req,res)=>{
- res.end('Hello from the server');
+    if(req.url === '/api' && req.method === 'GET'){
+ res.end('Hello from the server')
+    }
 })
 
 server.listen(PORT,()=> console.log(`server is listening on Port ${PORT}`));

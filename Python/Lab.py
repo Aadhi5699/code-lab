fe7854035f87d0ab2b7d508b6f458ecd28500732
Aadhi5699 <aadhi5699@gmail.com>
2026-06-16 16:08:10 +0530

    gather vs await

--- Python/Lab.py
+++ Python/Lab.py
@@ -6,3 +6,20 @@
 #     await hello()
 
 # asyncio.run(result())
+
+import asyncio
+
+async def task1():
+    await asyncio.sleep(3)
+
+async def task2():
+    await asyncio.sleep(3)
+
+async def main():
+    #both coroutines are scheduled immediately.
+    await asyncio.gather(
+        task1(),
+        task2()
+    )
+
+asyncio.run(main())

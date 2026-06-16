888e2afa59af83a47673bce5d3111919bd0bdc5b
Aadhi5699 <aadhi5699@gmail.com>
2026-06-16 19:20:39 +0530

    task

--- Python/Lab.py
+++ Python/Lab.py
@@ -7,19 +7,35 @@
 
 # asyncio.run(result())
 
+# import asyncio
+
+# async def task1():
+#     await asyncio.sleep(3)
+
+# async def task2():
+#     await asyncio.sleep(3)
+
+# async def main():
+#     #both coroutines are scheduled immediately.
+#     await asyncio.gather(
+#         task1(),
+#         task2()
+#     )
+
+# asyncio.run(main())
+
+
 import asyncio
 
-async def task1():
-    await asyncio.sleep(3)
-
-async def task2():
-    await asyncio.sleep(3)
+async def worker():
+    print("working")
+    await asyncio.sleep(2)
+    print("done") 
 
 async def main():
-    #both coroutines are scheduled immediately.
-    await asyncio.gather(
-        task1(),
-        task2()
-    )
+    #Creating a task does not guarantee completion.The event loop must stay alive.
+    asyncio.create_task(worker())
+    print("main finished")  
 
+#asyncio.run(main()) , only waits for main() to complete,the event loop shuts down.
 asyncio.run(main())

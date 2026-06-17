bab473087ee975dfc93d4d8f4e76ba0bf1d98550
Aadhi5699 <aadhi5699@gmail.com>
2026-06-17 21:44:31 +0530

    await asyncio.sleep(0)

--- Python/gather.py
+++ Python/gather.py
@@ -1,3 +1,4 @@
+#1
 import asyncio
 
 async def work(n):
@@ -9,5 +10,23 @@ async def main():
    #gather() preserves argument order
    results =  await asyncio.gather(work(3),work(1),work(2))
    print(results)
+
+asyncio.run(main())
+
+#2
+import asyncio
+
+async def worker(name):
+    print(f"{name}-1")
+#forces the currently running coroutine to pause and hand control back to the event loop
+    await asyncio.sleep(0) 
+
+    print(f"{name}-2")
+
+async def main():
+    await asyncio.gather(
+        worker("A"),
+        worker("B")
+    )
 
 asyncio.run(main())

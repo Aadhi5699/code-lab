81de3f58d373ded78df47416414d4da087382edf
Aadhi5699 <aadhi5699@gmail.com>
2026-06-21 15:24:23 +0530

    array of strings

--- JS/vowelCount.js
+++ JS/vowelCount.js
@@ -2,3 +2,21 @@ function getCount(str) {
   let vowels = ['a','e','i','o','u'];
   return str.split('').filter(letter=>vowels.includes(letter)).length;
 }
+
+function panic(str){
+    let result = [];
+    for(let i=0;i<str.length;i++){
+        if(str[i]=== " "){
+            result.push(" 😱 ");
+        }else{
+            result.push(str[i].toUpperCase());
+        }
+    }
+    result.push("!");
+    return result.join("");
+}
+
+
+// Test your function
+console.log(panic("I'm almost out of coffee")); 
+console.log(panic("winter is coming"))

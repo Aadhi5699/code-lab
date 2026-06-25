8c590427bae80f63e0fd0bb80f5bcfb204ec3da1
Aadhi5699 <aadhi5699@gmail.com>
2026-06-25 10:04:48 +0530

    FLatten array

--- JS/minmax.js
+++ JS/minmax.js
@@ -10,3 +10,18 @@ var max = function(list){
     list.sort((a, b) => (b - a));
     return list[0];
 }
+
+
+function flatten(arr){
+  let result=[];
+  for(let i=0;i<arr.length;i++){
+    if(Array.isArray(arr[i])){
+        for(let j=0;j<arr[i].length;j++){
+            result.push(arr[i][j]);
+        }
+    }else{
+        result.push(arr[i]);
+    }
+  }
+  return result;
+}

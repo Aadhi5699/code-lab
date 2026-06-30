ed30c0e1d0348dc13fd19016d7d1d7b9e9a9fc8a
Aadhi5699 <aadhi5699@gmail.com>
2026-06-30 12:50:27 +0530

    delete max one mismatch

--- JS/plaindrome.js
+++ JS/plaindrome.js
@@ -1,3 +1,5 @@
+//Handling alphanumeric
+
 var isPalindrome = function(s) {
    let left = 0;
    let right = s.length -1;
@@ -19,4 +21,31 @@ var isPalindrome = function(s) {
     right--;
    }
    return true;
+};
+
+// delete max one mismatch
+
+var validPalindrome = function(s) {
+    let left =0;
+    let right = s.length-1;
+
+    while(left < right){
+        if(s[left] !== s[right]){
+            return(isPalindrome(left+1 , right) || isPalindrome (left, right-1));
+        }
+         left++;
+         right --;
+    }
+    return true;
+
+    function isPalindrome(left, right){
+        while(left < right){
+            if(s[left] !== s[right]){
+                return false;
+            }
+            left++;
+            right --;
+        }
+        return true;
+    }
 };
